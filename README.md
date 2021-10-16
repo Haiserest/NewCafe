4cd03c21c08e05d940bb87811a7d2237b5b643c4
Haiserest <blach2542@hotmail.com>
2021-10-16 14:21:00 +0700

    update menu, api member

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NewCafe
 
 This repository Will make new UI Cafe blockchain
+
+
+API: https://www.mecallapi.com/api/users
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -9,3 +9,25 @@ btn.onclick = function(){
 search_btn.onclick = function(){
     sidebar.classList.toggle("active");
 }
+
+let greentea = document.querySelector("#Greentea");
+let katsudon = document.querySelector("#Katsudon");
+let purpletea = document.querySelector("#Purpletea");
+let spaghetti = document.querySelector("#Spaghetti");
+let mucha = document.querySelector("#Mucha");
+
+greentea.onclick = function(){
+    alert("Green tea : 0.011 eth");
+}
+katsudon.onclick = function(){
+    alert("Katsudon : 0.025 eth");
+}
+purpletea.onclick = function(){
+    alert("Purple tea : 0.014 eth");
+}
+spaghetti.onclick = function(){
+    alert("Spaghetti : 0.03 eth");
+}
+mucha.onclick = function(){
+    alert("Mucha : 0.015 eth");
+}
